261a66cc22c0d2dfaec0d4b441198cbcb757f43b
Dernbu <dernbu@gmail.com>
2021-11-20 01:07:33 +0800

    Finished all important '$'-related behavior regarding mathjax environments

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, moment, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { listenerCount } from 'process';
+import { moveCursor } from 'readline';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,70 +18,191 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		console.log("Loading!");
+		// this.registerEvent(this.app.workspace.on("editor-change", () =>{
+		// 	console.log("Editor Change!");
+		// 	const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 	const editor = view.editor;
+		// 	const cursor = editor.getCursor();
+			 
+		// 	const key = editor.getLine(cursor.line).charAt(cursor.ch-1);
+			
+		// 	console.log(key);
+			
+		// }));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		// this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
+		// 	console.log("Key Down!");
+		// 	console.log(evt.key);  
+		// });
+		
+		this.registerDomEvent(document, 'keypress', (event: KeyboardEvent) => {
+			console.log("Key Pressed!");
+			console.log(event.key); 
+			switch(event.key){
+				case '$':
+					this.handleDollarDown(event);
+					return;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		});		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerDomEvent(document, 'keyup', (evt: KeyboardEvent) => {
+		// 	console.log("Key Up!");
+		// });
+		
+		// this.addCommand({
+		// 	id: "test-id",
+		// 	name: "Insert $ symbols",
+		// 	editorCallback: (editor: Editor) => {
+		// 		const selection = editor.getSelection();
+		// 		editor.replaceSelection("$" + selection + "$");
+		// 	//   editor.replaceRange(moment().format("YYYY-MM-DD"), editor.getCursor());
+		// 		editor.setCursor(editor.getCursor().ch-2);
+		// 	},
+		//   });
+		
 	}
+	private readonly handleDollarDown = (
+		event: KeyboardEvent
+	): void => {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const editor = view.editor;
+		const cursor = editor.getCursor();
+		
+		const createInlineLatexEvn = (): void => {
+			const selection = editor.getSelection();
+			// Nothing selected
+			if(selection === ''){
+				editor.replaceSelection("$");
+				editor.setCursor(cursor.line, cursor.ch);
+			// Something selected
+			}else{
+				editor.replaceSelection("$" + selection);
+			}
+		}
+		const isMathEnv = (
+			lineToScan : String,
+			end: number
+			): Object => {
+			const isMathEnv = (
+				isInlineEnv : boolean, 
+				isEqnEnv : boolean,
+				prevCharIsDollar : boolean,
+				stringLeft: String): Object => {
+				
+				if(stringLeft.length == 0){
+					return {
+						inlineEnv: prevCharIsDollar? !isInlineEnv : isInlineEnv,
+						eqnEnv: isEqnEnv
+					};
+				}
+
+				const currentCharIsDollar = stringLeft.charAt(0) == "$";
+				const nextString = stringLeft.slice(1);
+
+				if(isEqnEnv){
+					if(!prevCharIsDollar){
+						return isMathEnv(false, true, currentCharIsDollar, nextString);
+					}else if(!currentCharIsDollar){
+						return isMathEnv(false, true, false, nextString);
+					}else{
+						return isMathEnv(false, false, false, nextString);
+					}
+
+				}
+
+				if(isInlineEnv){
+					// $ ... $$ --> math eqn env
+					if(currentCharIsDollar && prevCharIsDollar){
+						return isMathEnv(false, true, false, nextString);
+					
+					// $... $... --> look at next
+					}else if(currentCharIsDollar && !prevCharIsDollar){
+						return isMathEnv(true, false, true, nextString);
+					
+					// $ ... $<yay>
+					}else if(!currentCharIsDollar && prevCharIsDollar){
+						return isMathEnv(false, false, false, nextString);
+					
+					// $ ... <no $ in sight>
+					}else{
+						return isMathEnv(isInlineEnv, isEqnEnv, false, nextString);
+					}
+				}
+
+				// Not in inline or equation environment
+				if(!currentCharIsDollar && !prevCharIsDollar){
+					return isMathEnv(false, false, false, nextString);
+				}else if(currentCharIsDollar && !prevCharIsDollar){
+					return isMathEnv(false, false, true, nextString);
+				}else if(!currentCharIsDollar && prevCharIsDollar){
+					return isMathEnv(true, false, false, nextString);
+				}else{
+					return isMathEnv(false, true, false, nextString);
+				}
+
+
+			};
+			return isMathEnv(false, false, false, currentLine.slice(0, end));
+		};
+		const currentLine = editor.getLine(cursor.line);
+		const mathEnvStatus = isMathEnv(currentLine, cursor.ch);
+		console.log(mathEnvStatus);
+
+		// if(mathEnvStatus.inlineEnv){
+		// 	return;
+		// }
+		// if(mathEnvStatus.eqnEnv){
+		// 	return;
+		// }
+
+		// If not in a math environment,
+		const selection = editor.getSelection();
+		console.log("Char: " + currentLine.charAt(cursor.ch));
+		console.log("Char Before: " + currentLine.charAt(cursor.ch-1));
+		// Nothing selected
+		if(selection === ''){
+			
+			// $ ... |$ => $ ... $|, but not $|$ => $$|
+			// $$...$|$ => $$...$$|
+			if(mathEnvStatus.inlineEnv && currentLine.charAt(cursor.ch) == "$" && currentLine.charAt(cursor.ch-1) != "$"){
+				editor.setCursor({line:cursor.line, ch: cursor.ch+1});
+				editor.replaceRange("", {line:cursor.line, ch:cursor.ch}, {line:cursor.line, ch:cursor.ch+1});
+				return;
+			}
+
+			// $$...|$$ => $$...$|$
+			if(mathEnvStatus.eqnEnv && currentLine.slice(cursor.ch-1, cursor.ch+1) == "$$"){
+				editor.setCursor({line:cursor.line, ch: cursor.ch});
+				editor.replaceRange("", {line:cursor.line, ch:cursor.ch}, {line:cursor.line, ch:cursor.ch+1});
+				return;
+			}
+
+			// If 	$ ... $|, (| = cursor) then
+			// make $$ ... $$| 
+			if(!mathEnvStatus.inlineEnv && isMathEnv(currentLine, cursor.ch-1).inlineEnv){
+				const prevDollarIndex = currentLine.slice(0, cursor.ch-1).lastIndexOf("$");
+				editor.replaceRange("$", {line:cursor.line, ch:prevDollarIndex});
+				return;
+			}
+			
+			// Make $|$, also works for $|$ => $$|$$.
+			editor.replaceSelection("$");
+			editor.setCursor(cursor.line, cursor.ch);
+			return;
+			
+		// Something selected
+		}else{
+			editor.replaceSelection("$" + selection);
+			return;
+		}
+	};
 
 	onunload() {
-
+		console.log("Unloading!");
 	}
 
 	async loadSettings() {
@@ -89,6 +212,15 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	// private readonly handleKeyDown = (
+	// 	event: KeyboardEvent,
+	// ): void => {
+	// 	console.log("pressed!");
+	// }
+
+	
+
 }
 
 class SampleModal extends Modal {
@@ -120,7 +252,7 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Latex Toolkit for Obsidian - Settings'});
 
 		new Setting(containerEl)
 			.setName('Setting #1')
